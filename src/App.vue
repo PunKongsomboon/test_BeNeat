5ff9228dfeb10756cc7e2b5b141d814a99dc734f
Pun Kongsomboon <punkongsoomboon150@gmail.com>
2025-01-23 11:49:51 +0700

    initial project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HomePage from './views/HomePage.vue';
+
+</script>
+
+<template>
+  <div id="app">
+    <HomePage />
+
+    <router-view />
+  </div>
+</template>
+
+<style scoped></style>
